287c6497ea449e0de19baf1bd082267644f92d7b
Anikamythriyogesh <lathaica@gmail.com>
2021-01-22 15:30:23 +0530

    removed a log

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const Engine = Matter.Engine;
+iconst Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
@@ -28,9 +28,7 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
+      box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
